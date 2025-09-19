e425c6c40e7cbc4e5efa87b238578b0fc4e1095f
Philip Oyoo <philipoyoo11@gmail.com>
2025-09-19 11:42:19 +0300

    Update urls.py

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -16,6 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from .views import ConversationViewSet, MessageViewSet
+
+# ✅ Use DRF router to auto-generate routes
+router = routers.DefaultRouter()
+router.register(r"conversations", ConversationViewSet, basename="conversation")
+router.register(r"messages", MessageViewSet, basename="message")
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
